2fd1d0fc2e20a81a8313b8a6aa9cf85ce48c929e
Samuel Rodriguez <sam.rod.719@gmail.com>
2023-03-27 20:32:06 -0700

    get user with event method

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,6 +1,7 @@
 from flask_app.config.sqlconnection import connectToMySQL
 from flask_app import app
 from flask import flash
+from flask_app.models import event
 # from flask_bcrypt import Bcrypt
 # bcrypt = Bcrypt(app)
 import re
@@ -16,9 +17,11 @@ class User:
         self.password = data['password']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        self.events = []
 
     @classmethod
     def register_user(cls, data):
+        # does the data/form need tp be broken into a dictionary?
         query ="""INSERT INTO users (first_name, last_name, email, password) 
                 VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"""
         return connectToMySQL(cls.DB).query_db(query, data)
@@ -63,7 +66,7 @@ class User:
         return results
     
     @classmethod
-    def login_validation(cls, form):
+    def email_format_val(cls, form):
         is_valid = True
         if not EMAIL_REGEX.match(form['email']):
             is_valid = False
@@ -78,3 +81,25 @@ class User:
         query = "SELECT * FROM users WHERE users.id = %(user_id)s LIMIT 1;"
         results = connectToMySQL(cls.DB).query_db(query, data)
         return cls(results[0])
+
+    @classmethod
+    def get_user_with_events(cls, user_id):
+        data = {
+            "user_id": user_id
+        }
+        query = "SELECT * FROM users JOIN events ON events.user_id = users.id WHERE users.id = %(user_id)s"
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        user = cls(results[0])
+        for row in results:
+            event_info = {
+                "id": row['events.id'],
+                "name": row['name'],
+                "location": row['location'],
+                "date": row['date'],
+                "time": row['time'],
+                "created_at": row['events.created_at'],
+                "updated_at": row['events.updated_at']
+            }
+            one_event = event.Event(event_info)
+            user.events.append(one_event)
+        return user
